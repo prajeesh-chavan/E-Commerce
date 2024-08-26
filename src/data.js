3ecd73d1bda82cb16538262abd4881a7b5e7b3e8
Prathimesh-Jadhav <jadhavprathimesh2468@gmail.com>
2024-08-26 17:57:42 +0530

    removed scrollbar

--- src/data.js
+++ src/data.js
@@ -56,4 +56,28 @@ export default [
         "Quantity":3,
         "image":watch
     },
+    {
+        "title":"Titan x",
+        "Price":250,
+        "Quantity":3,
+        "image":watch
+    },
+    {
+        "title":"Titan x",
+        "Price":250,
+        "Quantity":3,
+        "image":watch
+    },
+    {
+        "title":"Titan x",
+        "Price":250,
+        "Quantity":3,
+        "image":watch
+    },
+    {
+        "title":"Titan x",
+        "Price":250,
+        "Quantity":3,
+        "image":watch
+    },
 ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary" : '#0b0b0b',
+        "primary": '#0b0b0b',
         "secondary": '#f6f6f6',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',     /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',               /* Chrome, Safari, and Opera */
+        },
+      });
+    },
+  ],
 };
