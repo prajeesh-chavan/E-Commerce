32d8666b1f1bce86664b20a1a1282a1485428c26
Prajeesh Chavan <prajeeshchavan@gmail.com>
2024-08-27 22:16:39 +0530

    minor changes

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useRef, useEffect } from "react";
-import { IoIosSearch } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward, IoIosSearch } from "react-icons/io";
 import ProductCard from "../components/ProductCard";
 import { IoClose } from "react-icons/io5";
 import { products } from "../data";
-
+import { Link } from "react-router-dom";
 
 function Products() {
   const [isFilterModalVisible, setIsFilterModalVisible] = useState(false);
   const [selectedBrands, setSelectedBrands] = useState([]);
   const [selectedColors, setSelectedColors] = useState([]);
   const [selectedSizes, setSelectedSizes] = useState([]);
-  const [priceRange, setPriceRange] = useState([500, 10000]); // Default price range
+  const [priceRange, setPriceRange] = useState([500, 10000]);
   const [selectedFeatures, setSelectedFeatures] = useState([]);
   const [isSortMenuVisible, setIsSortMenuVisible] = useState(false);
   const [selectedSortOption, setSelectedSortOption] = useState("");
   const [selectedFilterOption, setSelectedFilterOption] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(12); // No of products per page
 
   const sortMenuRef = useRef(null);
 
@@ -107,13 +109,25 @@ function Products() {
     setPriceRange([0, value]);
   };
 
+  // Pagination logic
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <>
-    {/* bar */}
+      {/* bar */}
       <div className="sticky top-0 z-10 bg-white shadow-md">
-        <div className="h-16 flex items-center p-5 lg:px-14 justify-between">
+        <div className="h-auto flex flex-col md:flex-row items-center p-5 lg:px-14 justify-between">
           <div className="relative" ref={sortMenuRef}>
-
             {/* Sort */}
             <button
               className="bg-white rounded-lg py-2 px-4 border-2 border-gray-200 hover:bg-gray-50 transition"
@@ -156,7 +170,7 @@ function Products() {
           {/* Search Bar */}
           <div className="relative text-gray-600">
             <input
-              className="border-2 border-gray-300 bg-white h-10 w-32 sm:w-64 lg:w-96 md:px-4 pr-10 rounded-lg text-md focus:outline-slate-400"
+              className="border-2 border-gray-300 bg-white h-10 w-32 sm:w-64 lg:w-96 px-4 pr-10 rounded-lg text-md focus:outline-slate-400"
               type="text"
               name="search"
               placeholder="Search"
@@ -184,11 +198,11 @@ function Products() {
       {/* Filter Modal */}
       {isFilterModalVisible && (
         <div className="fixed inset-0 bg-gray-500 bg-opacity-70 flex items-center justify-center z-30">
-          <div className="relative bg-white flex rounded-lg shadow-lg w-1/2 h-2/3">
+          <div className="relative bg-white flex rounded-lg shadow-lg w-5/6 h-3/4 lg:w-1/2 lg:h-2/3">
             {/* Side Menu */}
             <div className="w-1/4 bg-gray-100 rounded-s-lg">
               <h1 className="text-2xl p-4 font-bold border-b-2">Filter</h1>
-              <ul className="flex flex-col">
+              <ul className="flex flex-col text-sm md:text-base">
                 <li
                   className={`cursor-pointer p-2 ps-4 w-full hover:bg-gray-300 ${
                     selectedFilterOption === 1
@@ -246,7 +260,7 @@ function Products() {
             <IoClose
               onClick={closeFilterModal}
               size="24"
-              className="cursor-pointer absolute right-4 top-4 hover:shadow-md rounded-sm hover:scale-110 hover:bg-red-500 hover:text-white ease-in-out  duration-100"
+              className="cursor-pointer absolute right-4 top-4 hover:shadow-md rounded-sm hover:scale-110 hover:bg-red-500 hover:text-white ease-in-out duration-100"
             />
             <button
               className="absolute px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 bottom-5 right-5 hover:scale-105 hover:-translate-y-[0.1rem] ease-in duration-100"
@@ -256,7 +270,7 @@ function Products() {
             </button>
 
             {/* Filter Options */}
-            <div className="w-3/4 p-8">
+            <div className="w-3/4 p-8 pt-4">
               {/* Brand Filter */}
               {selectedFilterOption === 1 && (
                 <div className="mb-6">
@@ -338,8 +352,6 @@ function Products() {
                     <span>₹{priceRange[0]}</span>
                     <span>₹{priceRange[1]}</span>
                   </div>
-
-                  
                 </div>
               )}
 
@@ -373,21 +385,50 @@ function Products() {
         </div>
       )}
 
-      {/* Product Feed */}
-      <main className="h-full p-12 flex flex-col gap-6 justify-center">
-        <ul className="flex flex-wrap justify-evenly gap-10 list-none max-w-fit">
-          {products.map((product, index) => (
-            <li key={index}>
-              <ProductCard
-                title={product.title}
-                discount={product.discount}
-                price={product.price}
-                ratings={product.ratings}
-              />
-            </li>
-          ))}
-        </ul>
-      </main>
+      {/* Products */}
+      <div className="p-5 lg:px-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {currentProducts.map((product) => (
+          <Link to="/product-details">
+          <ProductCard
+            title={product.title}
+            discount={product.discount}
+            price={product.price}
+            ratings={product.ratings}
+          />
+          </Link>
+        ))}
+      </div>
+
+      {/* Pagination Controls */}
+      <div className="flex justify-center py-8">
+        <button
+          className="cursor-pointer hover:bg-gray-100 mx-2 px-2 py-2 rounded-l-lg"
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <IoIosArrowBack size="24" />
+        </button>
+        {[...Array(totalPages)].map((_, index) => (
+          <button
+            key={index}
+            className={`px-4 py-2 ${
+              index + 1 === currentPage
+                ? "bg-blue-500 text-white"
+                : "bg-white border border-gray-300 hover:bg-gray-50"
+            }  rounded-lg mx-1`}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          className="cursor-pointer hover:bg-gray-100 mx-2 px-2 py-2 rounded-r-lg"
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          <IoIosArrowForward size="24" />
+        </button>
+      </div>
     </>
   );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
   
 
 function ProductCard({title, discount, price, img, ratings}) {
   return (
     <>
-      <div class="relative flex w-full max-w-xs flex-col overflow-hidden rounded-lg bg-white border-gray-100 border-2 shadow-slate-100 shadow-lg hover:scale-[1.02] hover:-translate-y-2 ease-in-out delay-250 duration-300">
+      <div className="relative flex w-full max-w-xs flex-col overflow-hidden rounded-lg bg-white border-gray-100 border-2 shadow-slate-100 shadow-lg hover:scale-[1.02] hover:-translate-y-2 ease-in-out delay-250 duration-300">
         <a
-          class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
           href="#"
         >
           <img
@@ -28,11 +29,11 @@ function ProductCard({title, discount, price, img, ratings}) {
           <div class="mt-2 mb-5 flex items-center justify-between">
             <p>
               <span class="text-3xl font-bold text-slate-900">₹{price}</span>
-              {/* <span class="text-sm text-slate-900 line-through">$699</span> */}
+              {/* <span class="text-sm text-slate-900 line-through">₹6990</span> */}
             </p>
             {/* Ratings */}
             <div class="flex items-center">
-              <svg
+              {/* <svg
                 aria-hidden="true"
                 class="h-5 w-5 text-yellow-300"
                 fill="currentColor"
@@ -76,7 +77,7 @@ function ProductCard({title, discount, price, img, ratings}) {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-              </svg>
+              </svg> */}
               <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
                 {ratings}
               </span>
@@ -84,23 +85,11 @@ function ProductCard({title, discount, price, img, ratings}) {
           </div>
           <a
             href="#"
-            class="flex items-center justify-center rounded-md bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            class="flex items-center gap-2 justify-center rounded-md bg-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="mr-2 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />
-            </svg>
-            Add to cart
+            <HiOutlineShoppingCart size="24"/>
+
+            Add To Cart
           </a>
         </div>
       </div>
